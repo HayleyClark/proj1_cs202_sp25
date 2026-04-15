276c41997cfda1af3b14d79db10bb600f88f01dc
HayleyClark <hjclark@calpoly.edu>
2026-04-15 12:25:31 -0700

    Update proj1.py

--- proj1.py
+++ proj1.py
@@ -1,2 +1,19 @@
-git pull --no-rebase
+@dataclass(frozen=True)
+class GlobalRect:
+  lo_lat: float
+  hi_lat: float
+  west_long: float
+  east_long: float
 
+@dataclass(frozen=True)
+class Region:
+  rect: GlobalRect  #Physical boundaries
+  name: str        #Name of region
+  terrain: str     #Must be "ocean", "mountains", "forest", or "other"
+
+@dataclass(frozen=True)
+class RegionCondition:
+  region: Region      
+  year: int        #Year of observation
+  pop: int         #Population in that year
+  ghg_rate: float  #Greenhouse gas emission for that year
