95322b078a326617c4eab80b2a855c9d0c527bc7
HayleyClark <hjclark@calpoly.edu>
2026-04-15 21:51:40 -0700

    Update test_student.py

--- test_student.py
+++ test_student.py
@@ -6,9 +6,20 @@ from proj1 import *
 
 class TestRegionFunctions(unittest.TestCase):
 
-    def setUp(self):
-        pass
+    #emission_per_capita tests
+    def test_emissions_per_capita_normal(self):
+        rc = RegionCondition(Region(GlobeRect(0, 1, 0, 1), "Test", "other"), 2020, 3, 100.0)
+        self.assertAlmostEqual(emissions_per_capita(rc), 33.3333, places=4)
 
+    def test_emissions_per_capita_zero_population(self):
+        rc = RegionCondition(Region(GlobeRect(0, 1, 0, 1), "Test", "other"), 2020, 0, 200.0) 
+        self.assertEqual(emissions_per_capita(rc), 0.0)
+
+    def test_emissions_per_capita_zero_emissions(self):
+        rc = RegionCondition(Region(GlobeRect(0, 1, 0, 1), "Test", "other"), 2020, 100, 0.0) 
+        self.assertEqual(emissions_per_capita(rc), 0.0)
+
+    #
     def test_holder(self):
         pass
 
